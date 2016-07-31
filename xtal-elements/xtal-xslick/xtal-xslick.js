8e405954353c5fb5f220823871a2788cbfdc63be
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2016-07-31 04:48:58 -0400

    x-slickgrid

--- xtal-elements/xtal-xslick/xtal-xslick.js
+++ xtal-elements/xtal-xslick/xtal-xslick.js
@@ -1,4 +1,5 @@
 ///<reference path='../../bower_components/polymer/polymer.d.ts'/>
+///<reference path='js/SlickGrid.d.ts'/>
 var crystal;
 (function (crystal) {
     var elements;
@@ -9,7 +10,33 @@ var crystal;
                 //debugger;
             },
             ready: function () {
-                debugger;
+                var grid;
+                var columns = [
+                    { id: "title", name: "Title", field: "title" },
+                    { id: "duration", name: "Duration", field: "duration" },
+                    { id: "%", name: "% Complete", field: "percentComplete" },
+                    { id: "start", name: "Start", field: "start" },
+                    { id: "finish", name: "Finish", field: "finish" },
+                    { id: "effort-driven", name: "Effort Driven", field: "effortDriven" }
+                ];
+                var options = {
+                    enableCellNavigation: true,
+                    enableColumnReorder: false
+                };
+                $(function () {
+                    var data = [];
+                    for (var i = 0; i < 500; i++) {
+                        data[i] = {
+                            title: "Task " + i,
+                            duration: "5 days",
+                            percentComplete: Math.round(Math.random() * 100),
+                            start: "01/01/2009",
+                            finish: "01/05/2009",
+                            effortDriven: (i % 5 == 0)
+                        };
+                    }
+                    grid = new Slick.Grid("#myGrid", data, columns, options);
+                });
             }
         });
     })(elements = crystal.elements || (crystal.elements = {}));
