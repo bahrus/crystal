e8acc562e6de47279a6d8b2db4b3833bca3319c0
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2016-08-20 05:57:04 -0400

    exmple 5a

--- xtal-elements/x-slick-grid/js/DataViewHelper.js
+++ xtal-elements/x-slick-grid/js/DataViewHelper.js
@@ -3,12 +3,27 @@ var crystal;
 (function (crystal) {
     var elements;
     (function (elements) {
-        function createDataViewWithFilter(data, filter) {
+        function createDataViewWithFilter(data, filter, options) {
             var dataView = new Slick.Data.DataView({ inlineFilters: true });
             dataView.beginUpdate();
             dataView.setItems(data);
             dataView.setFilter(filter);
             dataView.endUpdate();
+            if (options && options.addStandardRowHandling) {
+                if (!options.gridFinder) {
+                    throw 'gridFinder required';
+                }
+                dataView.onRowCountChanged.subscribe(function (e, args) {
+                    var grid = options.gridFinder();
+                    grid.updateRowCount();
+                    grid.render();
+                });
+                dataView.onRowsChanged.subscribe(function (e, args) {
+                    var grid = options.gridFinder();
+                    grid.invalidateRows(args.rows);
+                    grid.render();
+                });
+            }
             return dataView;
         }
         elements.createDataViewWithFilter = createDataViewWithFilter;
--- xtal-elements/x-slick-grid/js/DataViewHelper.ts
+++ xtal-elements/x-slick-grid/js/DataViewHelper.ts
@@ -1,12 +1,32 @@
 ///<reference path='SlickGrid.d.ts'/>
 
 module crystal.elements{
-    export function createDataViewWithFilter(data, filter){
+    export interface ICreateDataViewOptions<T>{
+        addStandardRowHandling?:  boolean;
+        gridFinder?: () => Slick.Grid<T>;
+    }
+    export function createDataViewWithFilter<T>(data, filter, options?: ICreateDataViewOptions<T>){
         var dataView = new Slick.Data.DataView({ inlineFilters: true });
         dataView.beginUpdate();
         dataView.setItems(data);
         dataView.setFilter(filter);
         dataView.endUpdate();
+        if(options && options.addStandardRowHandling){
+            if(!options.gridFinder){
+                throw 'gridFinder required';
+            }
+            dataView.onRowCountChanged.subscribe(function (e, args) {
+                const grid = options.gridFinder();
+                grid.updateRowCount();
+                grid.render();
+            });
+
+            dataView.onRowsChanged.subscribe(function (e, args) {
+                const grid = options.gridFinder();
+                grid.invalidateRows(args.rows);
+                grid.render();
+            });
+        }
         return dataView;
     }
 }
