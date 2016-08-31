1787cbe123f14982604d6cce3107508b78479063
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2016-08-31 11:25:29 -0400

    reduce files

---
+++ xtal-elements/js-xtal-init.js
@@ -0,0 +1,40 @@
+///<reference path='../bower_components/polymer/polymer.d.ts'/>
+///<reference path='crystal.ts'/>
+var crystal;
+(function (crystal) {
+    var elements;
+    (function (elements) {
+        Polymer({
+            is: 'js-xtal-init',
+            properties: {
+                innerTarget: {
+                    type: String
+                },
+                allowNativeElementAsTarget: {
+                    type: Boolean
+                }
+            },
+            attached: function () {
+                var _this = this;
+                var actions = elements.evalInner(this);
+                var target = elements.nextNonScriptSibling(this);
+                if ((target && target['set']) || this.allowNativeElementAsTarget) {
+                    this.processTarget(target, actions);
+                }
+                else {
+                    this.async(function () {
+                        target = elements.nextDomBindElement(_this);
+                        _this.processTarget(target, actions);
+                    }, 1);
+                }
+            },
+            processTarget: function (target, actions) {
+                if (this.innerTarget) {
+                    target = target.querySelector(this.innerTarget);
+                }
+                elements.performCustElActions(actions, target);
+            }
+        });
+    })(elements = crystal.elements || (crystal.elements = {}));
+})(crystal || (crystal = {}));
+//# sourceMappingURL=js-xtal-init.js.map
