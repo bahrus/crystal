b1181c1a1311b06d37d98b046fa228a0ef46c2d4
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2017-02-20 08:05:51 -0500

    web component catalog

--- xtal-elements/WebComponentLibs.js
+++ xtal-elements/WebComponentLibs.js
@@ -10,7 +10,7 @@ var MajorVersion;
     MajorVersion[MajorVersion["V1"] = 1] = "V1";
 })(MajorVersion || (MajorVersion = {}));
 var libs = {
-    webcomponents_lite: {
+    webcomponents_lite_V0: {
         MajorVersion: MajorVersion.V0,
         CDNs: [
             {
@@ -20,6 +20,10 @@ var libs = {
                 infoURL: 'https://cdnjs.com/libraries/webcomponentsjs'
             }
         ]
+    },
+    webcomponents_lite_V1: {
+        MajorVersion: MajorVersion.V1,
+        CodeRepoUrl: 'https://github.com/webcomponents'
     },
     HTMLImports: {
         MajorVersion: MajorVersion.V0,
@@ -31,14 +35,35 @@ var libs = {
             }
         ]
     },
+    Polymer1: {
+        documentationUrl: 'https://www.polymer-project.org/1.0/',
+        CodeRepoUrl: 'https://github.com/Polymer/polymer',
+        NumberOfStars: 16979
+    },
     Sigil: {
-        CodeRepoUrl: 'https://github.com/sigiljs/',
+        CodeRepoUrl: 'https://github.com/sigiljs/sigil',
         cdn: {
             url: 'https://unpkg.com/sigiljs@0.0.20/sigil.min.js',
             kbSize: 5.4
         },
-        features: [wclf.htmlTemplateDef, wclf.attributeSupport, wclf.propertiesSupport]
+        features: [wclf.htmlTemplateDef, wclf.attributeSupport, wclf.propertiesSupport],
+        NumberOfStars: 15
     },
-    SlimJS: {}
+    SkateJS: {
+        CodeRepoUrl: 'https://github.com/skatejs/skatejs',
+        MajorVersion: MajorVersion.V1,
+        NumberOfStars: 1953
+    },
+    SlimJS: {
+        documentationUrl: 'http://slimjs.com/',
+        CodeRepoUrl: 'https://github.com/eavichay/slim.js',
+        NumberOfStars: 109
+    },
+    XTag1: {
+        MajorVersion: MajorVersion.V0,
+        CodeRepoUrl: 'https://github.com/x-tag/core',
+        documentationUrl: 'http://x-tag.github.io/',
+        NumberOfStars: 1095
+    }
 };
 //# sourceMappingURL=WebComponentLibs.js.map
--- xtal-elements/WebComponentLibs.ts
+++ xtal-elements/WebComponentLibs.ts
@@ -20,13 +20,14 @@ interface CDN {
 
 interface WebComponentLibrary{
     CodeRepoUrl?: string,
+    documentationUrl?: string,
     NumberOfStars?: number,
     MajorVersion?: MajorVersion,
     CDNs?: CDN[];
 }
 
 const libs = {
-    webcomponents_lite:{
+    webcomponents_lite_V0:{
         MajorVersion: MajorVersion.V0,
         CDNs:[
             {
@@ -36,6 +37,10 @@ const libs = {
                 infoURL: 'https://cdnjs.com/libraries/webcomponentsjs'
             }
         ]
+    },
+    webcomponents_lite_V1:{
+        MajorVersion: MajorVersion.V1,
+        CodeRepoUrl: 'https://github.com/webcomponents'
     },
     HTMLImports:{
         MajorVersion: MajorVersion.V0,
@@ -47,15 +52,34 @@ const libs = {
             }
         ]
     },
+    Polymer1:{
+        documentationUrl: 'https://www.polymer-project.org/1.0/',
+        CodeRepoUrl: 'https://github.com/Polymer/polymer',
+        NumberOfStars: 16979
+    },
     Sigil: {
-        CodeRepoUrl: 'https://github.com/sigiljs/',
+        CodeRepoUrl: 'https://github.com/sigiljs/sigil',
         cdn:{
             url: 'https://unpkg.com/sigiljs@0.0.20/sigil.min.js',
             kbSize: 5.4
         },
-        features:[wclf.htmlTemplateDef, wclf.attributeSupport, wclf.propertiesSupport]
+        features:[wclf.htmlTemplateDef, wclf.attributeSupport, wclf.propertiesSupport],
+        NumberOfStars: 15
     } as WebComponentLibrary,
+    SkateJS:{
+        CodeRepoUrl: 'https://github.com/skatejs/skatejs',
+        MajorVersion: MajorVersion.V1,
+        NumberOfStars: 1953
+    },
     SlimJS:{
-
+        documentationUrl: 'http://slimjs.com/',
+        CodeRepoUrl: 'https://github.com/eavichay/slim.js',
+        NumberOfStars: 109
+    },
+    XTag1:{
+        MajorVersion: MajorVersion.V0,
+        CodeRepoUrl: 'https://github.com/x-tag/core',
+        documentationUrl: 'http://x-tag.github.io/',
+        NumberOfStars: 1095
     }
 } as {[key: string] : WebComponentLibrary}
